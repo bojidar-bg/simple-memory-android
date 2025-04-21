4ff0df2649a1e869cd20fb0f4703af2e4e2f5c9c
Bojidar Marinov <bojidar.marinov.bg@gmail.com>
2025-04-21 22:16:02 +0300

    chore: Update path in signing script

--- sign.sh
+++ sign.sh
@@ -6,4 +6,4 @@ set -e
 
 zipalign -c -P 16 -v 4  ./build/outputs/apk/release/simple-memory-android-release-unsigned.apk
 
-apksigner sign --ks simple-memory-android.keystore --ks-key-alias memory --out simple-memory-android.apk ./build/outputs/apk/release/memory-grandpa-release-unsigned.apk
+apksigner sign --ks simple-memory-android.keystore --ks-key-alias memory --out simple-memory-android.apk ./build/outputs/apk/release/simple-memory-android-release-unsigned.apk
